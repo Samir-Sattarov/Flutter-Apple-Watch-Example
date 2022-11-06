9db766cfd0522902dcc82d5b1b24b4c5a6a3bd40
Samir-Sattarov <samirsattarov2004@gmail.com>
2022-11-06 09:20:20 +0500

    initial

---
+++ ios/testing Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var viewModel: WatchViewModel = WatchViewModel()
+    var body: some View {
+        VStack {
+                    Text("Counter: \(viewModel.counter)")
+                        .padding()
+                    Button(action: {
+                        viewModel.sendDataMessage(for: .sendCounterToFlutter, data: ["counter": viewModel.counter + 1])
+                    }) {
+                        Text("+ by 2")
+                    }
+                }
+         .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/testing Watch App/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing Watch App
+//
+//  Created by Samuel Sattarov on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct testing_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
